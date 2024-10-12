1152eef6ef9c3d152cbf9f735ca16e40b0016317
Alok Singh <alokbeniwal@gmail.com>
2024-10-12 13:52:21 -0700

    pivot

--- src/xai_hackathon/__init__.py
+++ src/xai_hackathon/__init__.py
@@ -1,2 +1,23 @@
-def hello() -> str:
-    return "Hello from xai-hackathon!"
+import xai_sdk
+from pathlib import Path
+"""
+~This module makes a heatmap of gazes on a room. It extracts faces from an image, and then uses a gaze estimation model to estimate the gaze direction of each face.~
+
+Scratch that, Grok doesn't OUTPUT images. 
+
+
+Since it's for the xAI hackathon, it's gonna use Grok.
+
+
+Images 
+
+"""
+PROJECT_ROOT = Path(__file__).parent
+
+# an image of a face, to get a bounding box
+test_image = PROJECT_ROOT / "test_image.jpg"
+
+
+def main()->None:
+    xai_sdk.run(test_image)
+
